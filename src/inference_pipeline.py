7a732bc697fbcbbc8730c7a009602c3394011c98
Ana Laura Diedrichs <analauradiedrichs@gmail.com>
2023-06-23 19:39:27 -0300

    fix #2

--- src/inference_pipeline.py
+++ src/inference_pipeline.py
@@ -1,5 +1,15 @@
 import subprocess
+import logging
+import logging.config
 
-subprocess.run(['Python', 'feature_engineering.py'])
 
-subprocess.run(['Python', 'predict.py'])
+logging.config.fileConfig(fname='log.conf', disable_existing_loggers=False)
+
+# Get the logger specified in the file
+logger = logging.getLogger(__name__)
+
+logger.debug('This is a debug message')
+
+subprocess.run(['python', 'feature_engineering.py'])
+
+subprocess.run(['python', 'predict.py'])
